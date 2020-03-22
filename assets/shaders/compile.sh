0ffad476d9bc6de7958b0bb36311c351ece3fd63
Tom Hauswald <t.hauswald@yahoo.de>
2020-03-22 14:39:12 +0100

    Add shader compile script.

---
+++ assets/shaders/compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p spirv
+for ext in vert frag; do
+	files=*.${ext}
+	numfiles=$(echo ${files} | wc -w)
+	echo "Compiling ${numfiles} ${ext} shaders..."
+	i=1
+	for file in ${files}; do
+		outfile=./spirv/${ext}-${file%%.*}.spv 
+		echo "    ${i}/${numfiles}: ${file} => ${outfile}"
+		glslc ${file} -o ${outfile}
+		i=$((i+1))
+	done
+	echo ""
+done
